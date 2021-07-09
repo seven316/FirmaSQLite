532601596b8d60b5e036a8a4a43adc666b73e4f8
siete316 <xseven316@gmail.com>
2021-07-08 23:16:32 -0600

    Agregar archivos de proyecto.

---
+++ FirmaSQLite/FirmaSQLite/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FirmaSQLite
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        Firmas itemFirmas = new Firmas();
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            try
+            {
+                ListaSGNT.ItemsSource = await App.InstanciaDB.ObtenerFirmas();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private async void BtnFirma_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PadPage());
+        }
+
+        private async void ListaFirmas_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+
+            itemFirmas = (Firmas)e.Item;
+
+            var jpg = itemFirmas.FirmaContent;
+            var nombre = itemFirmas.nombreFirma;
+            var descripcion = itemFirmas.descripcionFirma;
+
+
+            await Navigation.PushAsync(new firmasPage(jpg, nombre, descripcion));
+
+
+
+
+        }
+    }
+}
---
+++ FirmaSQLite/FirmaSQLite/firmasPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FirmaSQLite
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class firmasPage : ContentPage
+    {
+        public firmasPage(Byte[] imagen, string name, string desc)
+        {
+            InitializeComponent();
+
+
+            firmaIMG.Source = ImageSource.FromStream(() => new MemoryStream(imagen));
+
+            nameFirma.Text = name;
+            descFirma.Text = desc;
+
+        }
+    }
+}
